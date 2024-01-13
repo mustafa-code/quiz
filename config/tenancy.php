f46d03c7432ad0ea05050edf3a319f29f644cc88
Mustafa El-sir <tommy80080@gmail.com>
2024-01-14 01:02:12 +0300

    HotFix: Add localhost to central domains config

--- config/tenancy.php
+++ config/tenancy.php
@@ -20,7 +20,7 @@ return [
     'central_domains' => [
         // 'quizzes.test',
         '127.0.0.1',
-        // 'localhost',
+        'localhost',
         // 'tenant1.quiz.test',
         // 'tenant2.quiz.test',
     ],
