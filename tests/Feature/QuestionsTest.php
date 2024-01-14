504bad3e5b7f685c418c837b42ccf269ef5a91e4
Mustafa El-sir <tommy80080@gmail.com>
2024-01-14 15:43:38 +0300

    Test: Update question operation

--- tests/Feature/QuestionsTest.php
+++ tests/Feature/QuestionsTest.php
@@ -73,3 +73,59 @@ test('question can be created', function () {
     $this->assertDatabaseHas("questions", $data);
 });
 
+test('question edit page is displayed', function () {
+    $user = User::factory()->create();
+    $tenant = Tenant::factory()->create([
+        "user_id" => $user->id,
+    ]);
+    $quiz = Quiz::factory()->create([
+        "tenant_id" => $tenant->id,
+    ]);
+    $question = Question::factory()->create([
+        'tenant_id' => $tenant->id,
+        'quiz_id' => $quiz->id,
+    ]);
+
+    $response = $this
+        ->actingAs($user)
+        ->get(route("questions.edit", $quiz));
+
+    $response->assertOk();
+    $response->assertSee(route("questions.update", $quiz));
+    $response->assertSeeInOrder([$question->question, 
+        $question->slug, 
+        $question->description,
+    ]);
+    $response->assertViewIs("questions.edit");
+});
+
+test('question can be updated', function () {
+    $user = User::factory()->create();
+    $tenant = Tenant::factory()->create([
+        "user_id" => $user->id,
+    ]);
+    $quiz = Quiz::factory()->create([
+        "tenant_id" => $tenant->id,
+    ]);
+    $question = Question::factory()->create([
+        'tenant_id' => $tenant->id,
+        'quiz_id' => $quiz->id,
+    ]);
+
+    $data = [
+        "question" => "Updated Question",
+        "slug" => "updated-question",
+        "tenant_id" => $tenant->id,
+        "quiz_id" => $quiz->id,
+        "description" => "Updated Question Description",
+    ];
+    $response = $this
+        ->actingAs($user)
+        ->put(route("questions.update", $question), $data);
+
+    $response->assertSessionHasNoErrors();
+    $response->assertSessionHas('success', true);
+    $response->assertRedirect(route("questions.edit", $question));
+    $this->assertDatabaseHas("questions", $data);
+
+});
