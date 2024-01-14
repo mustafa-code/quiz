52a1daea8eabdb0261b9fe06404af247c8b1a994
Mustafa El-sir <tommy80080@gmail.com>
2024-01-14 15:12:08 +0300

    Test: Add question

--- tests/Feature/QuestionsTest.php
+++ tests/Feature/QuestionsTest.php
@@ -30,3 +30,46 @@ test('questions list page is displayed', function () {
     $response->assertViewIs("questions.index");
 });
 
+test('questions create page is displayed', function () {
+    $user = User::factory()->create();
+    $tenant = Tenant::factory()->create([
+        "user_id" => $user->id,
+    ]);
+
+    $response = $this
+        ->actingAs($user)
+        ->get(route("questions.create"));
+
+    $response->assertOk();
+    $response->assertSee(route("questions.store"));
+    $response->assertSee($tenant->name);
+    $response->assertViewIs("questions.create");
+});
+
+test('question can be created', function () {
+    $user = User::factory()->create();
+    $tenant = Tenant::factory()->create([
+        "user_id" => $user->id,
+    ]);
+    $quiz = Quiz::factory()->create([
+        "tenant_id" => $tenant->id,
+    ]);
+
+    $data = [
+        "question" => "Test Question",
+        "slug" => "test-question",
+        "tenant_id" => $tenant->id,
+        "quiz_id" => $quiz->id,
+        "description" => "Test Question Description",
+    ];
+
+    $response = $this
+        ->actingAs($user)
+        ->post(route("questions.store"), $data);
+
+    $response->assertSessionHasNoErrors();
+    $response->assertSessionHas('success', true);
+    $response->assertRedirect(route("questions.index"));
+    $this->assertDatabaseHas("questions", $data);
+});
+
