f5a5b59a4c48746d6f4cb1ca7bf2368faace1d11
Mustafa El-sir <tommy80080@gmail.com>
2024-01-14 10:50:20 +0300

    HotFix: Remove DeleteDatabase event when tenant deleted

--- app/Providers/TenancyServiceProvider.php
+++ app/Providers/TenancyServiceProvider.php
@@ -44,7 +44,8 @@ class TenancyServiceProvider extends ServiceProvider
             Events\DeletingTenant::class => [],
             Events\TenantDeleted::class => [
                 JobPipeline::make([
-                    Jobs\DeleteDatabase::class,
+                    // No need for these jobs if you're using the central db.
+                    // Jobs\DeleteDatabase::class,
                 ])->send(function (Events\TenantDeleted $event) {
                     return $event->tenant;
                 })->shouldBeQueued(false), // `false` by default, but you probably want to make this `true` for production.
