3b94352aca394c93a9c8cd9b4d43d5542b21f94a
Mustafa El-sir <tommy80080@gmail.com>
2024-01-14 10:44:13 +0300

    HotFix: Fix central domain issue

--- config/tenancy.php
+++ config/tenancy.php
@@ -18,9 +18,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        // 'quizzes.test',
-        '127.0.0.1',
-        'localhost',
+        env("TENANT_CENTER_DOMAIN", "localhost"),
     ],
 
     /**
