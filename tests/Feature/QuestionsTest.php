990bb408beb8e71cd88a2372a7bb54ceb02de471
Mustafa El-sir <tommy80080@gmail.com>
2024-01-14 13:56:41 +0300

    Enhance test for questions.index page

--- tests/Feature/QuestionsTest.php
+++ tests/Feature/QuestionsTest.php
@@ -2,16 +2,30 @@
 
 namespace Tests\Feature;
 
+use App\Models\Question;
+use App\Models\Quiz;
+use App\Models\Tenant;
 use App\Models\User;
 
 test('questions list page is displayed', function () {
     $user = User::factory()->create();
-
+    $tenant = Tenant::factory()->create([
+        "user_id" => $user->id,
+    ]);
+    $quiz = Quiz::factory()->create([
+        "tenant_id" => $tenant->id,
+    ]);
+    $question = Question::factory()->create([
+        'tenant_id' => $tenant->id,
+        'quiz_id' => $quiz->id,
+    ]);
+    
     $response = $this
         ->actingAs($user)
         ->get(route("questions.index"));
 
     $response->assertOk();
+    $response->assertSeeInOrder([$quiz->title, $question->question, $question->slug, $question->created_at]);
     $response->assertSee(route("questions.create"));
     $response->assertViewIs("questions.index");
 });
